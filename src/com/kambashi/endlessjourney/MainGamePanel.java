0dc65457397a5e4a351fb4f520585feea442350e
Seikun Kambashi <seikun_lee@hotmail.com>
2014-01-25 03:09:24 -0500

    Added hipster cyclops sprite

--- src/com/kambashi/endlessjourney/MainGamePanel.java
+++ src/com/kambashi/endlessjourney/MainGamePanel.java
@@ -4,22 +4,30 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import com.kambashi.endlessjourney.model.Bot;
+
 public class MainGamePanel extends SurfaceView implements
 		SurfaceHolder.Callback {
 
 	private static final String TAG = MainGamePanel.class.getSimpleName();
-	
+
 	private MainThread thread;
+	private Bot bot;
 
 	public MainGamePanel(Context context) {
 		super(context);
 		// adding the callback (this) to the surface holder to intercept events
 		getHolder().addCallback(this);
+
+		// create droid and load bitmap
+		bot = new Bot(BitmapFactory.decodeResource(getResources(),
+				R.drawable.droid_1), 50, 50);
 
 		// create the game loop thread
 		thread = new MainThread(getHolder(), this);
@@ -35,12 +43,17 @@ public class MainGamePanel extends SurfaceView implements
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
+		// at this point the surface is created and
+		// we can safely start the game loop
 		thread.setRunning(true);
 		thread.start();
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
+		Log.d(TAG, "Surface is being destroyed");
+		// tell the thread to shut down and wait for it to finish
+		// this is a clean shutdown
 		boolean retry = true;
 		while (retry) {
 			try {
@@ -50,23 +63,44 @@ public class MainGamePanel extends SurfaceView implements
 				// try again shutting down the thread
 			}
 		}
+		Log.d(TAG, "Thread was shut down cleanly");
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			// delegating event handling to the droid
+			bot.handleActionDown((int) event.getX(), (int) event.getY());
+
+			// check if in the lower part of the screen we exit
 			if (event.getY() > getHeight() - 50) {
 				thread.setRunning(false);
 				((Activity) getContext()).finish();
 			} else {
-				Log.d(TAG, "Coords: x=" + event.getX() + ", y=" + event.getY());
+				Log.d(TAG, "Coords: x=" + event.getX() + ",y=" + event.getY());
 			}
 		}
-		return super.onTouchEvent(event);
+		if (event.getAction() == MotionEvent.ACTION_MOVE) {
+			// the gestures
+			if (bot.isTouched()) {
+				// the droid was picked up and is being dragged
+				bot.setX((int) event.getX());
+				bot.setY((int) event.getY());
+			}
+		}
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+			// touch was released
+			if (bot.isTouched()) {
+				bot.setTouched(false);
+			}
+		}
+		return true;
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.droid_1), 10, 10, null);
+		// fills the canvas with black
+		canvas.drawColor(Color.BLACK);
+		bot.draw(canvas);
 	}
 }
